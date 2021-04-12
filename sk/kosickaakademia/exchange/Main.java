5770dbc060ae1e88df3baa0e083df0cf881e146d
Veronika Stanovská <veronika.stanovska@kosickaakademia.sk>
2021-04-12 11:43:00 +0200

    added gui

---
+++ sk/kosickaakademia/exchange/Main.java
@@ -0,0 +1,19 @@
+package sk.kosickaakademia.exchange;
+
+import sk.kosickaakademia.exchange.calc.CalcExchangeRates;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exchange rates");
+        Set<String> set = new HashSet<>();
+        set.add("USD");
+        set.add("HUF");
+        set.add("CZK");
+        set.add("BTC");
+        CalcExchangeRates calcExchangeRates = new CalcExchangeRates();
+        calcExchangeRates.calculate(90);
+    }
+}
---
+++ sk/kosickaakademia/exchange/gui/Main.java
@@ -0,0 +1,16 @@
+package sk.kosickaakademia.exchange.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
